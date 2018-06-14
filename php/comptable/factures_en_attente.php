ac76e20d2609ae140e30b6e9fba126665a244f98
qboudino <quentinpmf@gmail.com>
2018-06-14 18:24:44 +0200

    QB : factures en attente (non fonctionnel)

---
+++ php/comptable/factures_en_attente.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<?php
+//config
+include "../login/connectToBDD/conn.php";
+include '../../includes/config.php';
+?>
+
+<header id="header">
+    <?php include '../../includes/banner.php'; ?>
+    <?php include '../../includes/menu_comptable.php'; ?>
+</header>
+
+<body>
+<section class="section-gap">
+    <div class="title text-center">
+
+        <h1 style="margin-top: 70px" class="mb-10"><u>Factures à valider</u></h1>
+
+        <form method="post">
+            <table style="border: 1px solid black; border-collapse: separate; border-spacing: 5px;" align="center">
+
+                <?php
+                //récuperer infos table abonnements_utilisateurs
+                $allUtilisateurs=$bdd->prepare("SELECT * FROM utilisateurs WHERE id!=:UserId ORDER BY utilisateurs.nom ASC");
+                $allUtilisateurs->execute(array(
+                    'UserId'=>$_SESSION['UserId']
+                ));
+                while($data=$allUtilisateurs->fetch()){
+                    ?>
+                        <tr>
+                            <td align="left"><?php echo($data['nom']." ".$data['prenom'])?></td>
+                            <td align="left">
+                                <select name="select_factures_en_attente" id="select_factures_en_attente">
+                                    <option value="" selected></option>
+                                    <?php
+                                        $id_user = $data['id'];
+                                        $factures = $bdd->query("SELECT * FROM factures WHERE id_user='$id_user' AND statut=0");
+                                        while ($donnees_factures = $factures->fetch())
+                                        {
+                                            $id = htmlspecialchars($donnees_factures['id']);
+                                            $chemin = htmlspecialchars($donnees_factures['chemin']);
+                                            $date_creation = htmlspecialchars($donnees_factures['date_creation']);
+                                            $date_validation = htmlspecialchars($donnees_factures['date_validation']);
+                                            echo("<option value='$chemin'>"."Facture du ".$date_creation."</option>");
+
+                                        }
+                                    ?>
+
+                                </select>
+                            </td>
+                            <td align="right"><img onclick="window.location.href='/projetTA70/docs_client/FACT/'+$('#select_factures_en_attente :selected').text();;" src="../../img/loupe.jpg"></a></td>
+                        </tr>
+                    <?php
+                }
+                ?>
+
+            </table>
+        </form>
+
+    </div>
+</section>
+</body>
+
+</html>
