1b4b0574b5f117f9535a7c08da81e888e5ca86b5
quentin <quentinpmf@gmail.com>
2018-06-14 10:57:31 +0200

    QB : menu comptable

---
+++ includes/menu_comptable.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row align-items-center justify-content-center d-flex">
+        <nav id="nav-menu-container">
+            <ul class="nav-menu">
+                <li class="menu-active"><a href="home.php">Accueil</a></li>
+                <li class="menu-active"><a href="factures_en_attente.php">Factures en attente</a></li>
+                <li class="menu-active"><a href="factures_validees.php">Factures validées</a></li>
+            </ul>
+        </nav><!-- #nav-menu-container -->
+    </div>
+</div>
+<hr>
---
+++ php/comptable/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<?php
+//config
+include "../login/connectToBDD/conn.php";
+include '../../includes/config.php';
+?>
+
+<header id="header">
+    <?php include '../../includes/banner.php'; ?>
+    <?php include '../../includes/menu_comptable.php'; ?>
+</header>
+
+<body>
+<section class="section-gap">
+    <div class="title text-center"></div>
+</section>
+</body>
+
+</html>
