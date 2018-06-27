080fbf74b898aa4e6b94e04d7bb41ceeb9952875
qboudino <quentinpmf@gmail.com>
2018-06-27 13:35:39 +0200

    QB : remove jQuery JS/CSS + calendar styles adjust

--- includes/calendar/includeCSSandJScalendar.php
+++ includes/calendar/includeCSSandJScalendar.php
@@ -1,13 +1,27 @@
 <head>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css"/>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/locale-all.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/lang-all.js"></script>
+
+    <style>
+        .fc-content{
+            color:black;
+        }
+        .fc-content .fc-time{
+            color:white;
+        }
+        .fc-day-header{
+            background-color:#B93640;
+            color:white;
+        }
+        .fc-event{
+            background-color:#3B5FAB;
+            color:white;
+        }
+    </style>
 
 </head>
